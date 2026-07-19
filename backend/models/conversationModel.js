fd6b4a6717573b86278fd30126b418d7b73473d9
Vivekanand <vivekanand2721670@gmail.com>
2026-07-19 08:46:13 +0530

    Initial commit

---
+++ backend/models/conversationModel.js
@@ -0,0 +1,13 @@
+git commit -m "Initial commit"import mongoose from "mongoose";
+
+const conversationModel = new mongoose.Schema({
+    participants:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }],
+    messages:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Message"
+    }]
+},{timestamps:true});
+export const Conversation = mongoose.model("Conversation", conversationModel);
